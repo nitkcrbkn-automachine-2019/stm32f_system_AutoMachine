75c86dd6b5e2843555cb881ade224aab6e51dc82
UnknownSP <monsterhunter.sp.games@gmail.com>
2019-07-19 14:45:55 +0900

    add new file

---
+++ Drivers/DevDriver/Gene/Src/DD_Gene.c
@@ -0,0 +1,190 @@
+/* ===Kisarazu RBKN Library===
+ *
+ * autor          : Oishi
+ * version        : v0.10
+ * last update    : 20160703
+ *
+ * **overview***
+ * 共通部分のコードと他のレイヤーとの通信部分
+ *
+ * ・I2Cのみのサポート
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "app.h"
+#include "MW_I2C.h"
+#include "DD_Gene.h"
+#include "DD_MD.h"
+#include "DD_LD.h"
+#include "DD_AB.h"
+#include "DD_SS.h"
+#include "DD_ENCODER.h"
+#include "message.h"
+
+/*I2Cのサポート用関数*/
+int DD_I2C1Send(uint8_t add, const uint8_t *data, uint8_t size){
+  int ret = MW_I2C1Transmit(add, data, size);
+  if(ret)message("err","I2C1 trans faild \n addr:[%x],size:[%d],data:[0x%02x]",add,size,data[0]);
+  return ret;
+}
+int DD_I2C2Send(uint8_t add, const uint8_t *data, uint8_t size){
+  int ret = MW_I2C2Transmit(add, data, size);
+  if(!had_completed_tx)message("warning","I2C2 transmit had not completed");
+  if(ret)message("err","I2C2 trans faild \n addr:[%x],size:[%d],data:[0x%02x]",add,size,data[0]);
+  return ret;
+}
+int DD_I2C1Receive(uint8_t add, uint8_t *data, uint8_t size){
+  int ret = MW_I2C1Receive(add, data, size);
+  if(ret)message("err","I2C1 receive faild \n addr:[%x],size:[%d],data:[0x%02x]",add,size,data[0]);
+  return ret;
+}
+int DD_I2C2Receive(uint8_t add, uint8_t *data, uint8_t size){
+  int ret = MW_I2C2Receive(add, data, size);
+  if(!had_completed_rx)message("warning","I2C2 receive had not completed");
+  if(ret)message("err","I2C2 receive faild \n addr:[%x],size:[%d],data:[0x%02x]",add,size,data[0]);
+  return ret;
+}
+
+/*DeviceDriverのタスク*/
+int DD_doTasks(void){
+  int i;
+  int ret;
+#if DD_NUM_OF_MD
+  for( i = 0; i < DD_NUM_OF_MD; i++ ){
+    ret = DD_send2MD(&g_md_h[i]);
+    if( ret ){
+      return ret;
+    }
+  }
+#endif
+#if DD_NUM_OF_LD
+  for( i = 0; i < DD_NUM_OF_LD; i++ ){
+    ret = DD_send2LD(&g_ld_h[i]);
+    if( ret ){
+      return ret;
+    }
+  }
+#endif
+#if DD_NUM_OF_AB
+  for( i = 0; i < DD_NUM_OF_AB; i++ ){
+    ret = DD_send2AB(&g_ab_h[i]);
+    if( ret ){
+      return ret;
+    }
+  }
+#endif
+#if DD_NUM_OF_SV
+  ret = SV_SetRad(&g_sv_h);
+  if( ret ){
+      return ret;
+    }
+#endif
+#if DD_NUM_OF_SS
+  for(i=0; i<DD_NUM_OF_SS; i++){
+    ret = DD_SSPutReceiveRequest(i);
+    if( ret ){
+      return ret;
+    }
+  }
+  DD_receive2SS();
+#endif
+#if DD_USE_ENCODER1
+  ret = DD_encoder1update();
+  if( ret ){
+      return ret;
+    }
+#endif
+#if DD_USE_ENCODER2
+  ret = DD_encoder2update();
+  if( ret ){
+      return ret;
+    }
+#endif
+
+  return EXIT_SUCCESS;
+}
+
+/*Deviceのハンドラー表示用関数*/
+void DD_print(void){
+  int i;
+#if DD_NUM_OF_MD
+  for( i = 0; i < DD_NUM_OF_MD; i++ ){
+    DD_MDHandPrint(&g_md_h[i]);
+  }
+#endif
+#if DD_NUM_OF_LD
+  for( i = 0; i < DD_NUM_OF_LD; i++ ){
+    DD_LDHandPrint(&g_ld_h[i]);
+  }
+#endif
+#if DD_NUM_OF_AB
+  for( i = 0; i < DD_NUM_OF_AB; i++ ){
+    DD_ABHandPrint(&g_ab_h[i]);
+  }
+#endif
+#if DD_NUM_OF_SV
+  SV_print(&g_sv_h);
+#endif
+#if DD_NUM_OF_SS
+  for(i=0; i<DD_NUM_OF_SS; i++){
+    DD_SSHandPrint(&g_ss_h[i]);
+  }
+#endif
+#if DD_USE_ENCODER1 || DD_USE_ENCODER2
+  DD_encoderprint();
+#endif
+}
+
+/*初期化関数*/
+int DD_initialize(void){
+  int ret;
+  int i;
+
+  /* Initialize all configured peripherals */
+  MW_SetI2CClockSpeed(I2C1ID, _I2C_SPEED_BPS);
+  ret = MW_I2CInit(I2C1ID);
+  if( ret ){
+    return EXIT_FAILURE;
+  }
+
+  /*I2C2を用いたセンサを使用する場合、I2C2のInitを実行*/
+#if DD_NUM_OF_SS
+  MW_SetI2CClockSpeed(I2C2ID, _I2C_SPEED_BPS);
+  ret = MW_I2CInit(I2C2ID);
+  if( ret ){
+    return EXIT_FAILURE;
+  }
+#endif
+  
+#if DD_USE_ENCODER1
+  ret = DD_InitEncoder1();
+    if(ret)  {
+      message("error","Fialed initialize encoder1!");
+      return EXIT_FAILURE;
+    }
+#endif
+#if DD_USE_ENCODER2
+    ret = DD_InitEncoder2();
+    if(ret)  {
+      message("error","Fialed initialize encoder2!");
+      return EXIT_FAILURE;
+    }
+#endif
+
+#if DD_NUM_OF_SV
+  ret = SV_Init(&g_sv_h);
+  if( ret ){
+    return EXIT_FAILURE;
+  }
+#endif
+#if DD_NUM_OF_SS
+  for(i=0; i<DD_NUM_OF_SS; i++){
+    ret = SS_Init(&g_ss_h[i]);
+    if( ret ){
+      return EXIT_FAILURE;
+    }
+  }
+#endif
+  
+  return EXIT_SUCCESS;
+}
---
+++ Drivers/middleLayers/I2C/Inc/MW_I2C.h
@@ -0,0 +1,68 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 2016/06/02
+ *      Author: evaota
+ */
+
+#ifndef MIDLEWARE_INC_MW_I2C_H_
+#define MIDLEWARE_INC_MW_I2C_H_
+
+#include "stm32f1xx_hal.h"
+#include <stdbool.h>
+
+typedef enum
+  {
+    STANDARD,
+    HIGH
+  }speed_t;
+
+typedef enum
+  {
+    I2C1ID = 0,
+    I2C2ID = 1,
+  }i2cid_t;
+
+extern I2C_HandleTypeDef hi2c2;
+
+/*DMA送信完了フラグを定義*/
+static
+volatile bool had_completed_tx = true;
+
+/*DMA受信完了フラグを定義*/
+static
+volatile bool had_completed_rx = true;
+
+
+/**Saple code(main)
+ *
+ *
+ *
+ *
+ *
+ */
+
+/******************************* USART ID *******************************/
+#define IS_I2C_ID(ID) (((ID) == I2C1ID) ||	\
+		       ((ID) == I2C2ID))
+/**I2C1 GPIO Configuration
+   PB8     ------> I2C1_SCL
+   PB9     ------> I2C1_SDA
+*/
+
+/**I2C2 GPIO Configuration
+   PB10     ------> I2C2_SCL
+   PB11     ------> I2C2_SDA
+*/
+int MW_I2CInit(i2cid_t id);
+
+void MW_SetI2CClockSpeed(i2cid_t id,uint32_t ClockSpeed);
+
+int32_t MW_I2C1Transmit(uint8_t address,const uint8_t *data,uint16_t size);
+int32_t MW_I2C2Transmit(uint8_t address,const uint8_t *data,uint16_t size);
+int32_t MW_I2C1Receive(uint8_t address,uint8_t *data,uint16_t size);
+int32_t MW_I2C2Receive(uint8_t address,uint8_t *data,uint16_t size);
+void MW_I2C2TransitionCompletedCallBack(void);
+void MW_I2C2ReceptionCompletedCallBack(void);
+
+#endif /* MIDLEWARE_INC_MW_I2C_H_ */
